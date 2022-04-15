a121bcac2dd33e4d5694495911c83ee7cf901b34
shams <shams.orakzai.777@gmail.com>
2022-04-15 14:16:58 +0500

    Writing file asynchronously.

--- writing_file.js
+++ writing_file.js
@@ -1,11 +1,12 @@
-var file_stream = require("fs")
+var file_stream = require("fs")  // File stream object
 
+// Writing to file through file stream object.
 file_stream.writeFile("hello.txt", "Write this data on top of present data.", function (err) {
     if (err) {
-        console.log("Error while writitng File.", err);  
+        console.log("Error while writitng File.", err); // Error  
     } else {
-        console.log("Data written.");
+        console.log("Data written."); // Displaying message on console.
     }
 });
 
-console.log("Program ended.")
+console.log("Program ended.") // End of program
